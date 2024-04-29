7c1318683c40625eda8ab23440adeb2a53f22e1c
Newish0 <80089521+Newish0@users.noreply.github.com>
2024-04-28 18:39:41 -0700

    fix(backend api): changed dup shape logic

--- backend/api/src/services/gtfs-sync.ts
+++ backend/api/src/services/gtfs-sync.ts
@@ -5,7 +5,7 @@ import { routes } from "@/db/schemas/routes";
 import { trips } from "@/db/schemas/trips";
 import { shapes } from "@/db/schemas/shapes";
 import type defaultDB from "@/db";
-import { sql, eq } from "drizzle-orm";
+import { sql, eq, and } from "drizzle-orm";
 import {
     IndexColumn,
     PgInsertBase,
@@ -16,74 +16,89 @@ import { PostgresJsQueryResultHKT } from "drizzle-orm/postgres-js";
 import { equalRecords } from "@/utils/compare";
 
 export async function syncGtfsStaticWithPG(psDB: typeof defaultDB, gtfsDB: BetterSqlite3.Database) {
-    const inRoutes = getRoutes({}, [], [], { db: gtfsDB });
+    await psDB.transaction(async (tx) => {
+        const inRoutes = getRoutes({}, [], [], { db: gtfsDB });
 
-    for (const newRoute of inRoutes) {
-        const existingRoute = (
-            await psDB.select().from(routes).where(eq(routes.route_id, newRoute.route_id)).limit(1)
-        ).at(0);
+        for (const newRoute of inRoutes) {
+            const existingRoute = (
+                await tx
+                    .select()
+                    .from(routes)
+                    .where(eq(routes.route_id, newRoute.route_id))
+                    .limit(1)
+            ).at(0);
 
-        if (existingRoute) {
-            if (!equalRecords(existingRoute, newRoute)) {
-                console.log("SKIPPED! Existing route is different from new route");
-                console.log("Existing route:", existingRoute);
-                console.log("New route:", newRoute);
-                console.log();
+            if (existingRoute) {
+                if (!equalRecords(existingRoute, newRoute)) {
+                    console.log("SKIPPED! Existing route is different from new route");
+                    console.log("Existing route:", existingRoute);
+                    console.log("New route:", newRoute);
+                    console.log();
+                }
+            } else {
+                // console.log("New route:", newRoute);
+                await tx
+                    .insert(routes)
+                    .values(newRoute as typeof routes.$inferInsert)
+                    .onConflictDoNothing();
             }
-        } else {
-            console.log("New route:", newRoute);
-            await psDB
-                .insert(routes)
-                .values(newRoute as typeof routes.$inferInsert)
-                .onConflictDoNothing();
         }
-    }
 
-    const inTrips = getTrips({}, [], [], { db: gtfsDB });
+        const inTrips = getTrips({}, [], [], { db: gtfsDB });
 
-    for (const newTrip of inTrips) {
-        const existingTrip = (
-            await psDB.select().from(trips).where(eq(trips.trip_id, newTrip.trip_id)).limit(1)
-        ).at(0);
+        for (const newTrip of inTrips) {
+            const existingTrip = (
+                await tx.select().from(trips).where(eq(trips.trip_id, newTrip.trip_id)).limit(1)
+            ).at(0);
 
-        if (existingTrip) {
-            if (!equalRecords(existingTrip, newTrip)) {
-                console.log("SKIPPED! Existing trip is different from new trip");
-                console.log("Existing trip:", existingTrip);
-                console.log("New trip:", newTrip);
-                console.log();
+            if (existingTrip) {
+                if (!equalRecords(existingTrip, newTrip)) {
+                    console.log("SKIPPED! Existing trip is different from new trip");
+                    console.log("Existing trip:", existingTrip);
+                    console.log("New trip:", newTrip);
+                    console.log();
+                }
+            } else {
+                // console.log("New trip:", newTrip);
+                await tx
+                    .insert(trips)
+                    .values(newTrip as typeof trips.$inferInsert)
+                    .onConflictDoNothing();
             }
-        } else {
-            console.log("New trip:", newTrip);
-            await psDB
-                .insert(trips)
-                .values(newTrip as typeof trips.$inferInsert)
-                .onConflictDoNothing();
         }
-    }
 
-    const inShapes = getShapes({}, [], [], { db: gtfsDB });
+        const inShapes = getShapes({}, [], [], { db: gtfsDB });
 
-    for (const newShape of inShapes) {
-        const existingShape = (
-            await psDB.select().from(shapes).where(eq(shapes.shape_id, newShape.shape_id)).limit(1)
-        ).at(0);
+        for (const newShape of inShapes) {
+            const existingShape = (
+                await tx
+                    .select()
+                    .from(shapes)
+                    .where(
+                        and(
+                            eq(shapes.shape_id, newShape.shape_id),
+                            eq(shapes.shape_pt_sequence, newShape.shape_pt_sequence)
+                        )
+                    )
+                    .limit(1)
+            ).at(0);
 
-        if (existingShape) {
-            if (!equalRecords(existingShape, newShape)) {
-                console.log("SKIPPED! Existing shape is different from new shape");
-                console.log("Existing shape:", existingShape);
-                console.log("New shape:", newShape);
-                console.log();
+            if (existingShape) {
+                if (!equalRecords(existingShape, newShape)) {
+                    console.log("SKIPPED! Existing shape is different from new shape");
+                    console.log("Existing shape:", existingShape);
+                    console.log("New shape:", newShape);
+                    console.log();
+                }
+            } else {
+                // console.log("New shape:", newShape);
+                await tx
+                    .insert(shapes)
+                    .values(newShape as typeof shapes.$inferInsert)
+                    .onConflictDoNothing();
             }
-        } else {
-            console.log("New shape:", newShape);
-            await psDB
-                .insert(shapes)
-                .values(newShape as typeof shapes.$inferInsert)
-                .onConflictDoNothing();
         }
-    }
+    });
 }
 
 /**
