62eb625bd182758befa4fce287f6ebcdaf6844a8
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-01-05 00:44:15 -0800

    feat: better min count down & considers prev day late night services

--- clairvoyance-webapp/src/components/info/route-stop-stoptimes.tsx
+++ clairvoyance-webapp/src/components/info/route-stop-stoptimes.tsx
@@ -1,12 +1,18 @@
-import { createResource, For, Show, type Component } from "solid-js";
+import { createEffect, createResource, For, Show, type Component } from "solid-js";
 import { getRouteStopTimes } from "~/services/gtfs/stop_times";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 import { Card, CardContent } from "../ui/card";
-import { getArrivalMinutes } from "~/utils/time";
+import { decomposeTime, getArrivalMinutes, hhmmssToString } from "~/utils/time";
 import { ChevronRight, Triangle, TriangleRight } from "lucide-solid";
 import { cn } from "~/lib/utils";
 import RealTimeIndicator from "../ui/realtime-indicator";
 import OccupancyBadge from "../ui/occupancy-badge";
+import { addDays, addHours, format } from "date-fns";
+import type { StopTimeResponse } from "tmp-test/solid-trip-details/src/types";
+import type { RouteStopTimeResponse } from "~/services/gtfs/types";
+import { Separator } from "../ui/separator";
+import { buttonVariants } from "../ui/button";
+import { WheelGesturesPlugin } from "embla-carousel-wheel-gestures";
 
 type RouteStopStopTimesProps = {
     tripId: string;
@@ -15,73 +21,138 @@ type RouteStopStopTimesProps = {
 };
 
 const RouteStopStopTimes: Component<RouteStopStopTimesProps> = (props) => {
-    const [stopTimes] = createResource(
+    const [prevDayStopTimes] = createResource(
         () => ({ routeId: props.routeId, stopId: props.stopId }),
-        (params) => getRouteStopTimes({ ...params })
+        (params) =>
+            getRouteStopTimes({
+                ...params,
+                currentTime: (() => {
+                    const { hours, minutes, seconds } = decomposeTime(
+                        format(new Date(), "HH:mm:ss")
+                    );
+                    return hhmmssToString(hours + 24, minutes, seconds);
+                })(),
+                currentDate: format(addDays(new Date(), -1), "yyyyMMdd"),
+            })
     );
+    const [todayStopTimes] = createResource(
+        () => ({ routeId: props.routeId, stopId: props.stopId }),
+        (params) =>
+            getRouteStopTimes({
+                ...params,
+                currentTime: format(new Date(), "HH:mm:ss"),
+                currentDate: format(new Date(), "yyyyMMdd"),
+            })
+    );
+
+    createEffect(() => {
+        console.log("Today stop times", todayStopTimes());
+        console.log("Prev day stop times", prevDayStopTimes());
+    });
 
     return (
         <div>
-            <Carousel>
+            <Carousel plugins={[WheelGesturesPlugin()]}>
                 <CarouselContent>
-                    <For each={stopTimes()}>
-                        {(stopTime) => {
-                            const arrivalMinutes = getArrivalMinutes(
-                                stopTime.arrival_time,
-                                stopTime.realtime_arrival_delay ?? 0
-                            );
-
-                            return (
-                                <CarouselItem class="basis-1/2 lg:basis-1/3">
-                                    <div class="p-1">
-                                        <a
-                                            href={`/routes/${props.routeId}/trips/${stopTime.trip_id}/stops/${props.stopId}`}
-                                        >
-                                            <Card>
-                                                <CardContent
-                                                    class={cn(
-                                                        "p-2 relative flex flex-col gap-1 items-center justify-between",
-                                                        stopTime.trip_id !== props.tripId &&
-                                                            "opacity-75"
-                                                    )}
-                                                >
-                                                    <div
-                                                        class={cn(
-                                                            stopTime.trip_id === props.tripId
-                                                                ? "font-medium"
-                                                                : "font-light"
-                                                        )}
-                                                    >
-                                                        {arrivalMinutes} min
-                                                    </div>
-
-                                                    <div>
-                                                        <OccupancyBadge status="EMPTY" />
-                                                    </div>
-
-                                                    <Show
-                                                        when={
-                                                            stopTime.realtime_arrival_delay !==
-                                                            undefined
-                                                                ? stopTime.realtime_arrival_delay
-                                                                : null
-                                                        }
-                                                    >
-                                                        {(delay) => (
-                                                            <RealTimeIndicator delay={delay()} />
-                                                        )}
-                                                    </Show>
-                                                </CardContent>
-                                            </Card>
-                                        </a>
-                                    </div>
-                                </CarouselItem>
-                            );
-                        }}
+                    <For each={prevDayStopTimes()}>
+                        {(stopTime) => (
+                            <StopTimeItem
+                                routeId={props.routeId}
+                                tripId={props.tripId}
+                                stopId={props.stopId}
+                                stopTime={stopTime}
+                            />
+                        )}
                     </For>
+
+                    <For each={todayStopTimes()?.slice(0, 3)}>
+                        {(stopTime) => (
+                            <StopTimeItem
+                                routeId={props.routeId}
+                                tripId={props.tripId}
+                                stopId={props.stopId}
+                                stopTime={stopTime}
+                            />
+                        )}
+                    </For>
+
+                    <CarouselItem class="basis-1/2 lg:basis-1/3">
+                        <div class="p-1">
+                            <Card>
+                                <CardContent
+                                    class={cn(
+                                        "p-2 relative flex flex-col gap-1 items-center justify-between"
+                                    )}
+                                >
+                                    <a
+                                        class={buttonVariants({
+                                            variant: "link",
+                                        })}
+                                    >
+                                        Show more
+                                    </a>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </CarouselItem>
                 </CarouselContent>
             </Carousel>
         </div>
+    );
+};
+
+const StopTimeItem: Component<{
+    stopTime: RouteStopTimeResponse;
+    routeId: string;
+    stopId: string;
+    tripId: string;
+}> = (props) => {
+    const arrivalMinutes = getArrivalMinutes(
+        props.stopTime.arrival_time,
+        props.stopTime.realtime_arrival_delay ?? 0
+    );
+
+    return (
+        <CarouselItem class="basis-1/2 lg:basis-1/3">
+            <div class="p-1">
+                <a
+                    href={`/routes/${props.routeId}/trips/${props.stopTime.trip_id}/stops/${props.stopId}`}
+                >
+                    <Card>
+                        <CardContent
+                            class={cn(
+                                "p-2 relative flex flex-col gap-1 items-center justify-between",
+                                props.stopTime.trip_id !== props.tripId && "opacity-75"
+                            )}
+                        >
+                            <div
+                                class={cn(
+                                    props.stopTime.trip_id === props.tripId
+                                        ? "font-medium"
+                                        : "font-light"
+                                )}
+                            >
+                                {arrivalMinutes} min
+                            </div>
+
+                            <div>
+                                <OccupancyBadge status="EMPTY" />
+                            </div>
+
+                            <Show
+                                when={
+                                    props.stopTime.realtime_arrival_delay !== undefined
+                                        ? props.stopTime.realtime_arrival_delay
+                                        : null
+                                }
+                            >
+                                {(delay) => <RealTimeIndicator delay={delay()} />}
+                            </Show>
+                        </CardContent>
+                    </Card>
+                </a>
+            </div>
+        </CarouselItem>
     );
 };
 
--- clairvoyance-webapp/src/utils/time.ts
+++ clairvoyance-webapp/src/utils/time.ts
@@ -1,4 +1,4 @@
-import { parseISO, differenceInMinutes, differenceInSeconds } from "date-fns";
+import { parseISO, differenceInMinutes, differenceInSeconds, isPast, addSeconds } from "date-fns";
 
 import { parse, isValid, addDays } from "date-fns";
 
@@ -22,45 +22,127 @@ import { parse, isValid, addDays } from "date-fns";
  * parseTimeString('invalid')
  * // => { valid: false, originalTime: 'invalid' }
  */
-export function parseGTFSTimeString(gtfsTimeStr: string) {
-    const [hours, minutes, seconds] = gtfsTimeStr.split(":").map(Number);
-    if (isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
-        return { valid: false, originalTime: gtfsTimeStr };
-    }
+// export function parseGTFSTimeString(gtfsTimeStr: string) {
+//     const [hours, minutes, seconds] = gtfsTimeStr.split(":").map(Number);
+//     if (isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
+//         return { valid: false, originalTime: gtfsTimeStr };
+//     }
 
-    const normalizedTime = parse(
-        `${(hours % 24).toString().padStart(2, "0")}:${minutes
-            .toString()
-            .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`,
-        "HH:mm:ss",
-        // new Date(2024, 0, 1)
-        new Date()
-    );
+//     const normalizedTime = parse(
+//         `${(hours % 24).toString().padStart(2, "0")}:${minutes
+//             .toString()
+//             .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`,
+//         "HH:mm:ss",
+//         // new Date(2024, 0, 1)
+//         new Date()
+//     );
 
-    return isValid(normalizedTime)
-        ? {
-              valid: true,
-              originalTime: gtfsTimeStr,
-              normalized: normalizedTime,
-              extraDays: Math.floor(hours / 24),
-          }
-        : { valid: false, originalTime: gtfsTimeStr };
+//     return isValid(normalizedTime)
+//         ? {
+//               valid: true,
+//               originalTime: gtfsTimeStr,
+//               normalized: normalizedTime,
+//               extraDays: Math.floor(hours / 24),
+//           }
+//         : { valid: false, originalTime: gtfsTimeStr };
+// }
+
+/**
+ * Decomposes a time string into hours, minutes, and seconds
+ * @param time - Time string in format "HH:mm:ss"
+ * @returns Object containing hours, minutes, and seconds
+ */
+export function decomposeTime(time: string) {
+    const [hours, minutes, seconds] = time.split(":").map(Number);
+    return { hours, minutes, seconds };
 }
 
-export function getArrivalMinutes(arrivalTime: string, arrivalDelaySec: number = 0): number {
+/**
+ * Checks if a time string represents a late night service (24:00:00 or later)
+ * @param time - Time string in format "HH:mm:ss"
+ * @returns `true` if the time represents a late night service, `false` otherwise
+ * @throws {Error} If the time string is invalid
+ */
+function isLateNightService(time: string): boolean {
+    const { hours, minutes, seconds } = decomposeTime(time);
+    if (isNaN(hours) || isNaN(minutes) || isNaN(seconds)) {
+        throw new Error("Invalid time format. Expected HH:mm:ss");
+    }
+    return hours >= 24;
+}
+
+/**
+ * Calculates the number of extra days based on hours >= 24
+ * @param time - Time string in format "HH:mm:ss"
+ * @returns Number of extra days
+ * @throws {Error} If the time string is invalid
+ */
+function getNumberOfExtraDays(time: string): number {
+    const [hour] = time.split(":").map(Number);
+    if (isNaN(hour)) {
+        throw new Error("Invalid time format. Expected HH:mm:ss");
+    }
+    return Math.floor(hour / 24);
+}
+
+/**
+ * Normalizes a time string by converting hours >= 24 to equivalent 24-hour time
+ * @param time - Time string in format "HH:mm:ss"
+ * @returns Normalized time string in format "HH:mm:ss"
+ * @throws {Error} If the time string is invalid
+ */
+function getNormalizedTime(time: string): string {
+    const { hours: hour, minutes: min, seconds: sec } = decomposeTime(time);
+    if (isNaN(hour) || isNaN(min) || isNaN(sec)) {
+        throw new Error("Invalid time format. Expected HH:mm:ss");
+    }
+    if (min < 0 || min > 59 || sec < 0 || sec > 59) {
+        throw new Error("Invalid minutes or seconds. Must be between 0 and 59");
+    }
+    return hhmmssToString(hour % 24, min, sec);
+}
+
+/**
+ * Converts hours, minutes, and seconds to a formatted time string
+ * @param hour - Hours (0-Inf)
+ * @param min - Minutes (0-59)
+ * @param sec - Seconds (0-59)
+ * @returns Formatted time string in "HH:mm:ss" format
+ */
+export function hhmmssToString(hour: number, min: number, sec: number): string {
+    return `${hour.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}:${sec
+        .toString()
+        .padStart(2, "0")}`;
+}
+
+/**
+ * Calculates the number of minutes until an arrival time, accounting for delays
+ * and late night services
+ * @param arrivalTime - Time string in format "HH:mm:ss"
+ * @param arrivalDelaySec - Delay in seconds (positive for delays, negative for early arrivals)
+ * @param clamped - If true, negative values will be clamped to 0
+ * @returns Number of minutes until arrival
+ * @throws {Error} If the time string is invalid
+ */
+export function getArrivalMinutes(
+    arrivalTime: string,
+    arrivalDelaySec: number = 0,
+    clamped = true
+): number {
     const now = new Date();
+    const normalizedArrivalTime = parse(getNormalizedTime(arrivalTime), "HH:mm:ss", now);
+    const correctedArrivalTime = addSeconds(normalizedArrivalTime, arrivalDelaySec); // Add delay
 
-    const parsedArrivalTime = parseGTFSTimeString(arrivalTime);
-    console.log(arrivalTime, parseISO(arrivalTime), parsedArrivalTime);
+    if (isLateNightService(arrivalTime) && isPast(correctedArrivalTime)) {
+        const extraDays = getNumberOfExtraDays(arrivalTime);
+        correctedArrivalTime.setDate(correctedArrivalTime.getDate() + extraDays);
+    }
 
-    if (!parsedArrivalTime.valid || !parsedArrivalTime.normalized) return NaN;
+    const diffSeconds = differenceInSeconds(correctedArrivalTime, now);
 
-    const arrival = parsedArrivalTime.normalized;
-    const diffSeconds = differenceInSeconds(arrival, now);
-    const correctedDiffSeconds =
-        diffSeconds >= 0
-            ? diffSeconds
-            : differenceInSeconds(arrival.setDate(arrival.getDate() + 1), now);
+    if (clamped) {
+        return Math.max(0, Math.floor(diffSeconds / 60));
+    }
 
-    return Math.floor((correctedDiffSeconds + arrivalDelaySec) / 60);
+    return Math.floor(diffSeconds / 60);
 }
