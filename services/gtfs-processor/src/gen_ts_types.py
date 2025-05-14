c596c7b94770b1d4c808b7f6948deff2cc152b25
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-05-13 23:35:14 -0700

    feat: created auto gen TS types for db models

---
+++ services/gtfs-processor/src/gen_ts_types.py
@@ -0,0 +1,3 @@
+from pydantic_to_ts import generate_typescript_defs
+
+generate_typescript_defs("models", "./shared/gtfs-types.ts")
---
+++ services/gtfs-processor/src/pydantic_to_ts/__init__.py
@@ -0,0 +1,3 @@
+from .converter import generate_typescript_defs
+
+__all__ = ["generate_typescript_defs"]
