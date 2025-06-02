737d9fb5379f60d1b49f1977bcd25cfb7ba05878
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-06-01 23:00:49 -0700

    refactor: cleaned up frontend services

--- services/web-app/src/services/shapes.ts
+++ services/web-app/src/services/shapes.ts
@@ -1,11 +1,6 @@
-import EndpointEnv from "~/constants/endpoint-env";
 import { client } from "./client";
 
 export const getShapeAsGeoJson = async (shapeId: string) => {
-    // const res = await fetch(`${EndpointEnv.GTFS_API_ENDPOINT}/shapes/${shapeId}/geojson`);
-    // const json = await res.json();
-    // return json;
-
     // TODO: Error handling
     const res = await client.shapes({ shapeId }).geojson.get();
     return res.data;
