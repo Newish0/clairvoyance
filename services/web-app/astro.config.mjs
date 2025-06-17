547f0471d0f20f5f99c7ed302bf5908ed5656fdd
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-06-16 23:31:12 -0700

    feat: now a PWA

--- services/web-app/astro.config.mjs
+++ services/web-app/astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import solidJs from '@astrojs/solid-js';
-
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
-
 import node from '@astrojs/node';
+import AstroPWA from '@vite-pwa/astro';
+import pkgJson from './package.json';
 
 
 
@@ -15,14 +13,69 @@ const site = Bun.env.SITE || undefined;
 const base = Bun.env.BASE || "/";
 
 
-// TODO: Make this a PWA
-
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), sitemap(), tailwind({
-    applyBaseStyles: false
-  })],
+  integrations: [
+    solidJs(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    AstroPWA({
+      mode: 'development',
+      base: '/',
+      scope: '/',
+      includeAssets: ['favicon.ico'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: pkgJson.displayName,
+        short_name: pkgJson.name,
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          // {
+          //   src: 'icon-512-maskable.png',
+          //   sizes: '512x512',
+          //   type: 'image/png',
+          //   purpose: 'any maskable',
+          // },
+        ],
+        // screenshots: [
+        //   {
+        //     src: "img/mobile-screenshot1.png",
+        //     type: "image/png",
+        //     sizes: "540x720"
+        //   },
+        //   {
+        //     src: "img/mobile-screenshot2.png",
+        //     type: "image/png",
+        //     sizes: "540x720"
+        //   }
+        // ],
+      },
+      // workbox: {
+      //   navigateFallback: '/',
+      //   globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+      // },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/$/],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    })
+  ],
 
   site, base,
 
