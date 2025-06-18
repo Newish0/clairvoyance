c89c9b77bed1089692cd2cdf50768767f9ea4bab
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-06-18 00:19:38 -0700

    tmp fix: disable pwa until better fix

--- services/web-app/src/pwa.ts
+++ services/web-app/src/pwa.ts
@@ -1,14 +1,16 @@
 import { registerSW } from "virtual:pwa-register";
 
-registerSW({
-    immediate: true,
-    onRegisteredSW(swScriptUrl) {
-        console.log("SW registered: ", swScriptUrl);
-    },
-    onOfflineReady() {
-        console.log("PWA application ready to work offline");
-    },
-    onRegisterError(error) {
-        console.error("SW registration failed: ", error);
-    },
-});
+
+// FIXME: Disabled until we figure out why it's breaking client router... (likely to do with caching)
+// registerSW({
+//     immediate: true,
+//     onRegisteredSW(swScriptUrl) {
+//         console.log("SW registered: ", swScriptUrl);
+//     },
+//     onOfflineReady() {
+//         console.log("PWA application ready to work offline");
+//     },
+//     onRegisterError(error) {
+//         console.error("SW registration failed: ", error);
+//     },
+// });
