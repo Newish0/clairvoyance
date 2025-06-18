9c62851de74aac4b1094ec3f4096e8c3caad321b
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-06-18 00:19:38 -0700

    tmp fix: disable pwa until better fix

--- services/web-app/astro.config.mjs
+++ services/web-app/astro.config.mjs
@@ -4,8 +4,8 @@ import solidJs from '@astrojs/solid-js';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
-import AstroPWA from '@vite-pwa/astro';
-import pkgJson from './package.json';
+// import AstroPWA from '@vite-pwa/astro';
+// import pkgJson from './package.json';
 
 
 
@@ -22,59 +22,61 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false
     }),
-    AstroPWA({
-      mode: 'development',
-      base: '/',
-      scope: '/',
-      includeAssets: ['favicon.ico'],
-      registerType: 'autoUpdate',
-      manifest: {
-        name: pkgJson.displayName,
-        short_name: pkgJson.name,
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          // {
-          //   src: 'icon-512-maskable.png',
-          //   sizes: '512x512',
-          //   type: 'image/png',
-          //   purpose: 'any maskable',
-          // },
-        ],
-        // screenshots: [
-        //   {
-        //     src: "img/mobile-screenshot1.png",
-        //     type: "image/png",
-        //     sizes: "540x720"
-        //   },
-        //   {
-        //     src: "img/mobile-screenshot2.png",
-        //     type: "image/png",
-        //     sizes: "540x720"
-        //   }
-        // ],
-      },
-      // workbox: {
-      //   navigateFallback: '/',
-      //   globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
-      // },
-      devOptions: {
-        enabled: true,
-        navigateFallbackAllowlist: [/^\/$/],
-      },
-      experimental: {
-        directoryAndTrailingSlashHandler: true,
-      },
-    })
+
+    // FIXME: Disabled until we figure out why it's breaking client router... (likely to do with caching)
+    // AstroPWA({
+    //   mode: 'development',
+    //   base: '/',
+    //   scope: '/',
+    //   includeAssets: ['favicon.ico'],
+    //   registerType: 'autoUpdate',
+    //   manifest: {
+    //     name: pkgJson.displayName,
+    //     short_name: pkgJson.name,
+    //     theme_color: '#ffffff',
+    //     icons: [
+    //       {
+    //         src: 'icon-192.png',
+    //         sizes: '192x192',
+    //         type: 'image/png',
+    //       },
+    //       {
+    //         src: 'icon-512.png',
+    //         sizes: '512x512',
+    //         type: 'image/png',
+    //       },
+    //       // {
+    //       //   src: 'icon-512-maskable.png',
+    //       //   sizes: '512x512',
+    //       //   type: 'image/png',
+    //       //   purpose: 'any maskable',
+    //       // },
+    //     ],
+    //     // screenshots: [
+    //     //   {
+    //     //     src: "img/mobile-screenshot1.png",
+    //     //     type: "image/png",
+    //     //     sizes: "540x720"
+    //     //   },
+    //     //   {
+    //     //     src: "img/mobile-screenshot2.png",
+    //     //     type: "image/png",
+    //     //     sizes: "540x720"
+    //     //   }
+    //     // ],
+    //   },
+    //   // workbox: {
+    //   //   navigateFallback: '/',
+    //   //   globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+    //   // },
+    //   devOptions: {
+    //     enabled: true,
+    //     navigateFallbackAllowlist: [/^\/$/],
+    //   },
+    //   experimental: {
+    //     directoryAndTrailingSlashHandler: true,
+    //   },
+    // })
   ],
 
   site, base,
