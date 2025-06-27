579b01f161fd5a53c18ff3d0ccd57c6ab090fc16
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-06-27 00:16:53 -0700

    feat: main map now uses imperative style

--- services/web-app/src/components/maps/main-map.tsx
+++ services/web-app/src/components/maps/main-map.tsx
@@ -1,87 +1,129 @@
-import { createEffect, createSignal, on, Show, type Component } from "solid-js";
-import { Marker, type Viewport } from "solid-map-gl";
+import { createEffect, createSignal, on, onCleanup, onMount, Show, type Component } from "solid-js";
 import { useMapLocation } from "~/hooks/use-map-location";
 import { cn } from "~/lib/utils";
-import BaseMap from "../ui/base-map";
-import { debounce } from "@solid-primitives/scheduled";
+import maplibregl from "maplibre-gl";
+import { useMapStyle } from "~/hooks/use-map-style";
+import { isFpEqual } from "~/utils/numbers";
+import { createCustomZoomController } from "~/lib/map/custom-zoom-controller";
 
 const MainMap: Component = () => {
+    let container: HTMLDivElement;
+    const [map, setMap] = createSignal<maplibregl.Map | null>(null);
+
+    const mapStyle = useMapStyle();
+    const markers = {
+        currentLocation: new maplibregl.Marker({
+            element: (
+                <div
+                    class={cn("rounded-full w-6 h-6 border-4", "bg-sky-600/95", "border-white/90")}
+                ></div>
+            ) as HTMLDivElement,
+        }).setLngLat([0, 0]),
+        selectedLocation: new maplibregl.Marker({
+            element: (
+                <div
+                    class={cn(
+                        "rounded-full w-6 h-6 border-4",
+                        "bg-purple-600/95",
+                        "border-white/90"
+                    )}
+                ></div>
+            ) as HTMLDivElement,
+        }).setLngLat([0, 0]),
+    } as const;
+
     const mapLocation = useMapLocation({
         thresholdDistance: 100,
         enableHighAccuracy: true,
     });
 
-    const [viewport, setViewport] = createSignal({
-        // Format: [lon, lat]
-        center: [0, 0],
-        zoom: 14,
-    } as Viewport);
-
-    const handleViewportChange = (evt: Viewport) => {
-        setViewport(evt);
-        setViewport((prev) => ({ ...prev, center: evt.center }));
-
-        const center = evt.center;
-        mapLocation.setSelectedLocation({
-            lng: center[0],
-            lat: center[1],
+    onMount(() => {
+        const map = new maplibregl.Map({
+            container: container,
+            style: mapStyle(),
+            center: [0, 0], // Don't care; init in effect below
+            zoom: 14,
         });
-    };
 
+        map.once("load", () => {
+            setMap(map);
+        });
+
+        map.on("move", () => {
+            const center = map.getCenter();
+
+            // Ensure selected location marker (NOT the actual selected location) is
+            // always at the center of the map. (This is for the marker animation; UI only)
+            markers.selectedLocation.setLngLat(center);
+        });
+
+        map.on("moveend", () => {
+            const center = map.getCenter();
+
+            // Only update the selected location when using finish selecting the location
+            // to not constantly trigger map set center from side effect.
+            mapLocation.setSelectedLocation({
+                lng: center.lng,
+                lat: center.lat,
+            });
+        });
+    });
+
+    onCleanup(() => {
+        map()?.remove();
+    });
+
+    // Sync map center with selected location. This ensures the map is always at the selected location.
+    // NOTE: if using geolocation (aka current location), the geolocation is still sync to selected location
+    //       given the user has attached the selected location to the current geolocation (see `useMapLocation`)
     createEffect(
-        on([mapLocation.selectedLocation], ([selectedLocation]) => {
-            if (selectedLocation) {
-                setViewport({
-                    ...viewport(),
-                    center: [selectedLocation.lng, selectedLocation.lat],
-                });
+        on([mapLocation.selectedLocation, map], ([selectedLocation, map]) => {
+            if (selectedLocation && map) {
+                // Don't move the map if it's already at the selected location
+                const mapCenter = map.getCenter();
+                if (
+                    isFpEqual(selectedLocation.lng, mapCenter.lng, 1e-32) &&
+                    isFpEqual(selectedLocation.lat, mapCenter.lat, 1e-32)
+                ) {
+                    console.log("Map already at selected location");
+                    return;
+                }
+
+                console.log("Moving map to selected location", selectedLocation);
+
+                map.setCenter([selectedLocation.lng, selectedLocation.lat]);
             }
         })
     );
 
-    return (
-        <BaseMap viewport={viewport()} onViewportChange={handleViewportChange}>
-            {/* User GPS location marker  */}
-            <Show when={mapLocation.currentLocation()}>
-                {(currentLocation) => (
-                    <Marker
-                        lngLat={[currentLocation().lng, currentLocation().lat]}
-                        options={{
-                            element: (
-                                <div
-                                    class={cn(
-                                        "rounded-full w-6 h-6 border-4",
-                                        "bg-sky-600/95",
-                                        "border-white/90"
-                                    )}
-                                ></div>
-                            ),
-                        }}
-                    ></Marker>
-                )}
-            </Show>
+    // Sync markers with selected location and current location
+    createEffect(
+        on(
+            [
+                map,
+                mapLocation.selectedLocation,
+                mapLocation.showSelectedMarker,
+                mapLocation.currentLocation,
+            ],
+            ([map, selectedLocation, showSelectedMarker, currentLocation]) => {
+                if (!map) return;
 
-            {/* Show selected location if there's no GPS location or selected location is different from GPS location */}
-            <Show when={mapLocation.showSelectedMarker() && mapLocation.selectedLocation()}>
-                {(selectedLocation) => (
-                    <Marker
-                        lngLat={[selectedLocation().lng, selectedLocation().lat]}
-                        options={{
-                            element: (
-                                <div
-                                    class={cn(
-                                        "rounded-full w-6 h-6 border-4",
-                                        "bg-purple-600/95",
-                                        "border-white/90"
-                                    )}
-                                ></div>
-                            ),
-                        }}
-                    ></Marker>
-                )}
-            </Show>
-        </BaseMap>
+                if (currentLocation) {
+                    markers.currentLocation.setLngLat([currentLocation.lng, currentLocation.lat]);
+                    markers.currentLocation.addTo(map);
+                }
+
+                // Controls selected location (purple marker) visibility
+                if (showSelectedMarker && selectedLocation) {
+                    markers.selectedLocation.addTo(map);
+                } else {
+                    markers.selectedLocation.remove();
+                }
+            }
+        )
     );
+
+    return <div ref={container} class="w-full h-full"></div>;
 };
 
 export default MainMap;
