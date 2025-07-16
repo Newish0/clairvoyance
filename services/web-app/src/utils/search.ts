e44d36c36ec370e1f1501901bab554d670f5c301
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-07-15 19:54:14 -0700

    fix: don't error out on invalid schedule

--- services/web-app/src/utils/search.ts
+++ services/web-app/src/utils/search.ts
@@ -1,4 +1,4 @@
-export const mostCommonValue = <T>(values: T[]) => {
+export const mostCommonValue = <T>(values: T[]): T | undefined => {
     const counts = new Map<T, number>();
     for (const value of values) {
         const count = counts.get(value) ?? 0;
@@ -7,5 +7,5 @@ export const mostCommonValue = <T>(values: T[]) => {
     return counts
         .entries()
         .toArray()
-        .sort((a, b) => b[1] - a[1])[0][0];
+        .sort((a, b) => b[1] - a[1])?.[0]?.[0];
 };
