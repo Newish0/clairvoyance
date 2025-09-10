bedbb81e5046e22884ad5b48666dd578c9f73d21
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-09-09 22:26:00 -0700

    feat: Added shapes ingest pipeline

--- services/gtfs-processor/src/ingest_pipeline/transforms/gtfs/shape_mapper.py
+++ services/gtfs-processor/src/ingest_pipeline/transforms/gtfs/shape_mapper.py
@@ -1,9 +1,10 @@
-from typing import AsyncIterator, Dict
+from collections import defaultdict
+from dataclasses import dataclass, field
+from typing import AsyncIterator, Dict, List, Tuple
 from ingest_pipeline.core.errors import ErrorPolicy
 from models.mongo_schemas import Shape, LineStringGeometry
 from pymongo import UpdateOne
 from ingest_pipeline.core.types import Context, Transformer
-from beanie.odm.operators.update.general import Set
 
 
 class ShapeMapper(Transformer[Dict[str, str], UpdateOne]):
@@ -13,25 +14,51 @@ class ShapeMapper(Transformer[Dict[str, str], UpdateOne]):
     Output: Mongo UpdateOne
     """
 
+    @dataclass
+    class __TmpShapeInfo:
+        geometry: Dict[int, Tuple[float, float]] = field(default_factory=dict)
+        distances_traveled: Dict[int, float] = field(default_factory=dict)
+
+    __tmp_shapes: Dict[str, __TmpShapeInfo]
+
     def __init__(self, agency_id: str):
         self.agency_id = agency_id
+        self.__tmp_shapes = defaultdict(self.__TmpShapeInfo)
 
     async def run(
         self, context: Context, items: AsyncIterator[Dict[str, str]]
     ) -> AsyncIterator[UpdateOne]:
         async for row in items:
             try:
-                shape_doc = Shape(
+
+                pt_lat = (
+                    float(row.get("shape_pt_lat")) if "shape_pt_lat" in row else None
+                )
+                pt_lon = (
+                    float(row.get("shape_pt_lon")) if "shape_pt_lon" in row else None
+                )
+                distances_traveled = (
+                    float(row.get("shape_dist_traveled"))
+                    if "shape_dist_traveled" in row
+                    else None
+                )
+                pt_sequence = (
+                    int(row.get("shape_pt_sequence"))
+                    if "shape_pt_sequence" in row
+                    else None
+                )
+
+                validation_shape_doc = Shape(
                     agency_id=self.agency_id,
+                    shape_id=row.get("shape_id"),
+                    geometry=LineStringGeometry(coordinates=[(pt_lon, pt_lat)]),
+                    distances_traveled=[float(distances_traveled)],
                 )
 
-                # TODO: Figure out how to upsert shapes b/c we store the entire geometry as a LineString
+                tmp_shape = self.__tmp_shapes[validation_shape_doc.shape_id]
+                tmp_shape.geometry[pt_sequence] = (pt_lon, pt_lat)
+                tmp_shape.distances_traveled[pt_sequence] = distances_traveled
 
-                yield UpdateOne(
-                    {"agency_id": self.agency_id, "shape_id": shape_doc.shape_id},
-                    {"$set": shape_doc.model_dump(exclude={"id"})},
-                    upsert=True,
-                )
             except Exception as e:
                 match context.error_policy:
                     case ErrorPolicy.FAIL_FAST:
@@ -42,3 +69,37 @@ class ShapeMapper(Transformer[Dict[str, str], UpdateOne]):
                         continue
                     case _:
                         raise e
+
+        try:
+            shape_docs = self.__get_constructed_shape()
+            for shape_doc in shape_docs:
+                yield UpdateOne(
+                    {"agency_id": self.agency_id, "shape_id": shape_doc.shape_id},
+                    {"$set": shape_doc.model_dump(exclude={"id"})},
+                    upsert=True,
+                )
+        except Exception as e:
+            match context.error_policy:
+                case ErrorPolicy.FAIL_FAST:
+                    raise e
+                case ErrorPolicy.SKIP_RECORD:
+                    context.telemetry.incr(f"shape_mapper.skipped")
+                    context.logger.error(e)
+                case _:
+                    raise e
+
+    def __get_constructed_shape(self):
+        for shape_id, tmp_shape in self.__tmp_shapes.items():
+            yield Shape(
+                agency_id=self.agency_id,
+                shape_id=shape_id,
+                geometry=LineStringGeometry(
+                    coordinates=[
+                        tmp_shape.geometry[i] for i in sorted(tmp_shape.geometry.keys())
+                    ]
+                ),
+                distances_traveled=[
+                    tmp_shape.distances_traveled[i]
+                    for i in sorted(tmp_shape.distances_traveled.keys())
+                ],
+            )
