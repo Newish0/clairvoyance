5f5ff11407818481fba2debbda48587789e0d294
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-09-25 23:54:45 -0700

    Fix: Add default min max date range to trip instance pipeline

--- services/gtfs-processor/src/ingest_pipeline/pipelines/gtfs/trip_instances_pipeline.py
+++ services/gtfs-processor/src/ingest_pipeline/pipelines/gtfs/trip_instances_pipeline.py
@@ -9,7 +9,7 @@ from models.mongo_schemas import TripInstance
 
 
 def build_trip_instances_pipeline(
-    agency_id, min_date, max_date, log_level=logging.INFO
+    agency_id, min_date="00000101", max_date="99991231", log_level=logging.INFO
 ):
     stages = [
         StageSpec(
