028b812f8f7c7039167c4f0ca7d0e18d8f73b0fb
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-10-09 21:48:17 -0700

    Update home-map.tsx

--- transit-app/src/components/maps/home-map.tsx
+++ transit-app/src/components/maps/home-map.tsx
@@ -1,32 +1,40 @@
 import { ProtoMap } from "@/components/maps/proto-map";
 import { trpc } from "@/main";
 import { useQuery } from "@tanstack/react-query";
-import { useCallback, useEffect, useState } from "react";
+import { use, useCallback, useEffect, useRef, useState } from "react";
 import { Marker, useMap, type ViewStateChangeEvent } from "react-map-gl/maplibre";
-import { debounce } from "@/lib/utils";
+import { useDebounce } from "@uidotdev/usehooks";
 
 export const HomeMap = () => {
     const [viewState, setViewState] = useState({
         longitude: -123.35,
         latitude: 48.47,
-        zoom: 14,
+        zoom: 15,
     });
 
-    const { data, refetch } = useQuery({
-        ...trpc.stop.getNearby.queryOptions({
-            lat: viewState.latitude,
-            lng: viewState.longitude,
-            radius: 1500,
-        }),
+    const [nearbyStopQueryParams, setNearbyStopQueryParams] = useState({
+        lat: viewState.latitude,
+        lng: viewState.longitude,
+        radius: 1500,
     });
+    const debouncedNearbyStopQueryParams = useDebounce(nearbyStopQueryParams, 300);
 
-    const debouncedRefetch = useCallback(() => debounce(refetch, 1000), [refetch]);
+    const { data } = useQuery({
+        ...trpc.stop.getNearby.queryOptions(debouncedNearbyStopQueryParams),
+        enabled: viewState.zoom > 13,
+    });
 
     return (
         <ProtoMap
             {...viewState}
             onMove={(evt) => setViewState(evt.viewState)}
-            onMoveEnd={() => debouncedRefetch()}
+            onMoveEnd={() =>
+                setNearbyStopQueryParams({
+                    lat: viewState.latitude,
+                    lng: viewState.longitude,
+                    radius: Math.min((18 - viewState.zoom) * 100, 5000), // TODO: Use bbox instead
+                })
+            }
         >
             {data?.map((stop) => (
                 <Marker
