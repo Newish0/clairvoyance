94d62a8b76bac06e8b9e80ea9d5d66bc8c00624c
Newish0 <80089521+Newish0@users.noreply.github.com>
2025-10-28 00:26:15 -0700

    Feat[App]: selectable location and user geolocation

--- transit-app/src/components/maps/proto-map.tsx
+++ transit-app/src/components/maps/proto-map.tsx
@@ -1,20 +1,32 @@
-import Map from "react-map-gl/maplibre";
-import "maplibre-gl/dist/maplibre-gl.css";
+import { DEFAULT_LOCATION } from "@/constants/location";
 import { useProtoMapsStyle } from "@/hooks/use-map-style";
+import "maplibre-gl/dist/maplibre-gl.css";
 import type { ComponentProps } from "react";
+import Map, { AttributionControl } from "react-map-gl/maplibre";
 
 export const ProtoMap = (props: ComponentProps<typeof Map>) => {
     const mapStyle = useProtoMapsStyle();
+
     return (
         <Map
             initialViewState={{
-                longitude: -123.35,
-                latitude: 48.47,
+                longitude: DEFAULT_LOCATION.lng,
+                latitude: DEFAULT_LOCATION.lat,
                 zoom: 14,
             }}
             {...props}
             style={{ width: "100%", height: "100%" }}
             mapStyle={mapStyle}
-        ></Map>
+            attributionControl={false}
+        >
+            <AttributionControl
+                position="bottom-left"
+                compact={true}
+                style={{
+                    opacity: 0.7,
+                }}
+            />
+            {props.children}
+        </Map>
     );
 };
