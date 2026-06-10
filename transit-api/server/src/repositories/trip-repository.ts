aef1ccab0eda1227d5eff524d09b0a290bc3e8fb
Newish0 <80089521+Newish0@users.noreply.github.com>
2026-06-09 20:19:40 -0700

    added generalized findDepartures

--- transit-api/server/src/repositories/trip-repository.ts
+++
@@ -1,9 +0,0 @@
-import { trips } from "database";
-import { eq } from "drizzle-orm";
-import { DataRepository } from "./data-repository";
-
-export class TripRepository extends DataRepository {
-    public async findById(tripId: typeof trips.$inferSelect.id) {
-        return this.db.select().from(trips).where(eq(trips.id, tripId));
-    }
-}
