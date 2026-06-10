4ec21151cc89a9a019b8942d5c48d9afc67eb766
Newish0 <80089521+Newish0@users.noreply.github.com>
2026-06-10 00:40:48 -0700

    fix: one db client to limit too many client err

--- transit-api/server/src/context.ts
+++ transit-api/server/src/context.ts
@@ -2,10 +2,9 @@ import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import { getDb } from "./db";
 
 const DATABASE_URL = process.env.DATABASE_URL!;
+const db = getDb(DATABASE_URL);
 
 export async function createContext(opts: FetchCreateContextFnOptions) {
-    const db = getDb(DATABASE_URL);
-
     return {
         db,
     };
