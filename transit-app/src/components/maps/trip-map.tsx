68108a066619dd5a13f2b246b63132da34f2b14b
Newish0 <80089521+Newish0@users.noreply.github.com>
2026-07-01 02:11:58 -0700

    Improv: proper zoom and center btw user loc and stop

--- transit-app/src/components/maps/trip-map.tsx
+++ transit-app/src/components/maps/trip-map.tsx
@@ -1,9 +1,10 @@
 import { ProtoMap } from "@/components/maps/proto-map";
 import { DEFAULT_LOCATION } from "@/constants/location";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { usePersistUserSetLocation } from "@/hooks/use-persist-user-set-location";
 import type { Direction } from "database/models/enums";
-import React, { useCallback, useEffect, useState } from "react";
-import { type ViewStateChangeEvent } from "react-map-gl/maplibre";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { type MapRef, type ViewStateChangeEvent } from "react-map-gl/maplibre";
 import AppMapSideControls from "./controls/app-map-side-controls";
 import { ShapeLayer } from "./layers/shape-layer";
 import { StopsLayer } from "./layers/stops-layer";
@@ -25,6 +26,8 @@ export type TripMapProps = {
 export const TripMap: React.FC<TripMapProps> = ({ tripData, routeColor, routeTextColor }) => {
     const targetStop = tripData?.stopInfos.find((stop) => stop.isTarget);
 
+    const mapRef = useRef<MapRef>(null);
+    const isMobile = useIsMobile();
     const [viewState, setViewState] = useState({
         longitude: DEFAULT_LOCATION.lng,
         latitude: DEFAULT_LOCATION.lat,
@@ -33,22 +36,47 @@ export const TripMap: React.FC<TripMapProps> = ({ tripData, routeColor, routeTex
 
     const [userSetLocation] = usePersistUserSetLocation();
 
-    // Set the map center to the mid point between the stop and the user set location.
-    // If there is no stop location, use the user set location.
     useEffect(() => {
-        const roughMidLng = targetStop?.lng
-            ? (targetStop.lng + userSetLocation.lng) / 2
-            : userSetLocation.lng;
-        const roughMidLat = targetStop?.lat
-            ? (targetStop.lat + userSetLocation.lat) / 2
-            : userSetLocation.lat;
-
         setViewState((prev) => ({
             ...prev,
-            longitude: roughMidLng,
-            latitude: roughMidLat,
+            longitude: userSetLocation.lng,
+            latitude: userSetLocation.lat,
         }));
-    }, [targetStop, userSetLocation, setViewState]);
+    }, [userSetLocation, setViewState]);
+
+    // Fit the map to include both the target stop and the user set location.
+    const handleLoad = () => {
+        const map = mapRef.current?.getMap();
+        map?.resize();
+
+        const camera =
+            targetStop?.lng && targetStop?.lat
+                ? map?.cameraForBounds(
+                      [
+                          [
+                              Math.min(targetStop.lng, userSetLocation.lng),
+                              Math.min(targetStop.lat, userSetLocation.lat),
+                          ],
+                          [
+                              Math.max(targetStop.lng, userSetLocation.lng),
+                              Math.max(targetStop.lat, userSetLocation.lat),
+                          ],
+                      ],
+                      {
+                          padding: 80,
+                          maxZoom: 16,
+                          offset: [isMobile ? 0 : window.innerWidth / 4, -100],
+                      },
+                  )
+                : null;
+
+        if (camera)
+            map?.flyTo({
+                ...camera,
+                animate: true,
+                duration: 1000,
+            });
+    };
 
     const handleMove = useCallback(
         (evt: ViewStateChangeEvent) => {
@@ -58,7 +86,7 @@ export const TripMap: React.FC<TripMapProps> = ({ tripData, routeColor, routeTex
     );
 
     return (
-        <ProtoMap {...viewState} onMove={handleMove}>
+        <ProtoMap {...viewState} ref={mapRef} onMove={handleMove} onLoad={handleLoad}>
             <AppMapSideControls />
 
             {tripData && (
